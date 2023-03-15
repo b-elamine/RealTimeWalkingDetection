7618a6d0ea3a6e59160ac8d9d112251aeaed3604
bechorfa <m.bechorfa@esi-sba.dz>
2023-03-15 18:16:47 +0100

    Initialization..

--- app/src/main/java/com/example/walkingdetection/MainActivity.java
+++ app/src/main/java/com/example/walkingdetection/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.walkingdetection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView result, welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = findViewById(R.id.result);
+        result.setText("Results of the activity");
     }
+
 }
