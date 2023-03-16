4a629c1bbd0397f0103f7122b60461ca75ffaae4
bechorfa <m.bechorfa@esi-sba.dz>
2023-03-16 11:03:58 +0100

    Setting accelerometer sensor up

--- app/src/main/java/com/example/walkingdetection/MainActivity.java
+++ app/src/main/java/com/example/walkingdetection/MainActivity.java
@@ -1,20 +1,78 @@
 package com.example.walkingdetection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 
-    TextView result, welcome;
+    TextView result;
+    private Sensor accelerometer;
+    private SensorManager sensorManager;
+    private boolean sensorPresent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         result = findViewById(R.id.result);
         result.setText("Results of the activity");
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED) { //ask for permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+            }
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            sensorPresent = true;
+        } else {
+            result.setText("No sensor found !");
+            sensorPresent = false;
+        }
+
+
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            result.setText(String.valueOf(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+
+            sensorManager.registerListener(this,
+                    accelerometer,
+                    SensorManager.SENSOR_DELAY_GAME);
+        } else {
+            Toast.makeText(this, "Sensor Not found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
