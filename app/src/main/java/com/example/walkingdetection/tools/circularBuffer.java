a946667e57643a7f679686153143e1240ca7b1aa
bechorfa <m.bechorfa@esi-sba.dz>
2023-03-22 14:02:18 +0100

    Revert "Initializing processing"
    This reverts commit ad46c9291c6da157a1959de3a822916a05904222.

--- app/src/main/java/com/example/walkingdetection/tools/circularBuffer.java
+++ app/src/main/java/com/example/walkingdetection/tools/circularBuffer.java
@@ -35,27 +35,12 @@ public class circularBuffer {
     }
 
     private void processWindow() {
+        float[] window = toArray();
+        // Do processing on window here
+        // ...
+        Long start = System.nanoTime(); // for calculating execution time
 
-        // Standard Deviation
         float sd = calculateStandardDeviation(toArray());
-
-        // Calculating AFC
-        List<Float> window = new ArrayList<Float>(toArray().length);
-        for (float f : toArray()){
-            window.add(f);
-        }
-        List<Float> afc_window = afc(window,window.size());
-
-        // Peaks Detection
-        float i =0;
-        List<Float> index = new ArrayList<>();
-        for (float f : afc_window
-             ) {
-            index.add(i++);
-        }
-        Pair<List<Float>, List<Float>> peaks = peaksDetect(afc_window, 3.0E-01f, index);
-
-
         // Update tail to create overlap with next window
         tail = (tail + windowSize - overlapSize) % buffer.length;
         size -= (windowSize - overlapSize);
@@ -215,6 +200,4 @@ public class circularBuffer {
         }
         return new Pair<>(positionMax, maxPeaks);
     }
-
-
 }
