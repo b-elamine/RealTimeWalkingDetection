60c3f4fb4cdc346c78204437ff60101ae6f2aaf5
bechorfa <m.bechorfa@esi-sba.dz>
2023-03-23 16:36:41 +0100

    Plotting Magnitude realtime

--- app/src/main/java/com/example/walkingdetection/MainActivity.java
+++ app/src/main/java/com/example/walkingdetection/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -18,6 +19,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.walkingdetection.tools.circularBuffer;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.text.DecimalFormat;
 
@@ -30,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     private boolean sensorPresent;
     private circularBuffer buffer;
     private SensorThread sensorThread;
+    private LineChart mChart;
+    private LineChart mChartSd;
 
 
     @Override
@@ -58,9 +69,115 @@ public class MainActivity extends AppCompatActivity {
 
         // SensorThread instance
         sensorThread = new SensorThread((SensorManager) getSystemService(Context.SENSOR_SERVICE), buffer);
-        sensorThread.start();
         //result.setText("Standard deviation : "+buffer.getProcessedData().getStandardDeviation());
+
+        mChart = (LineChart) findViewById(R.id.chart);
+        mChartSd = (LineChart) findViewById(R.id.chartsd);
+
+        init_Charts(mChart);
+
+        sensorThread.start(); //start collecting processing and plotting ..
+
     }
+
+    private void init_Charts(LineChart chart){
+        // enable description text
+        chart.getDescription().setEnabled(true);
+
+        // enable touch gestures
+        chart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(true);
+
+        // set an alternative background color
+        chart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        chart.setData(data);
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = chart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMaximum(10f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        chart.getAxisLeft().setDrawGridLines(false);
+        chart.getXAxis().setDrawGridLines(false);
+        chart.setDrawBorders(false);
+    }
+
+    private void addEntry(SensorEvent event) {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
+            data.addEntry(new Entry(set.getEntryCount(), (float) Math.sqrt(Math.pow(event.values[0],2)+Math.pow(event.values[1],2)+Math.pow(event.values[2],2))-5), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(150);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(1.5f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+
+
 
     @Override
     protected void onDestroy() {
@@ -104,12 +221,16 @@ public class MainActivity extends AppCompatActivity {
                 DecimalFormat df = new DecimalFormat();
                 df.setMaximumFractionDigits(3);
                 result.setText("STD : " + df.format(buffer.getProcessedData().getStandardDeviation()));
-                System.out.println("_______________std_______________   " + df.format(buffer.getProcessedData().getStandardDeviation()) + "   " + buffer.getProcessedData().getStepCount());
+                System.out.println("_______________std_______________   " +
+                        df.format(buffer.getProcessedData().getStandardDeviation()) +
+                        "   "
+                        + buffer.getProcessedData().getStepCount());
             }
 
+            addEntry(event);
+
+
         }
-
-
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy) {
